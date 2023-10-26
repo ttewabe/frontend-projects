c724b8953b49a37788deb4308d497fed399adc0d
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-10-26 15:09:11 -0700

    convert number to word

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const [result, setResult] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const handleClick = () => {
+      if (buttonRef.current) {
+        buttonRef.current.style.color = "red";
+      }
+    };
+
+    if (buttonRef.current) {
+      buttonRef.current.addEventListener("click", handleClick);
+    }
+  }, []);
+
+  useEffect(() => {
+    setResult(`${TextAndNumber(count)} ${count} `);
+  }, [count]);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleTextToggle = () => {
+    setResult("Count Cells With Text");
+  };
+
+  const handleTextRemove = () => {
+    setResult("");
+  };
+
+  // Function to convert a number to its text representation
+  const TextAndNumber = (number: any) => {
+    const textArray = [
+      "Zero:-",
+      "One",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+    ];
+    const numDigits = String(number).split("").map(Number);
+
+    if (numDigits.length === 0) {
+      return "zero";
+    }
+
+    return numDigits.map((number) => textArray[number]).join(" ");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="container">
+          <h1>Increment</h1>
+          <button ref={buttonRef} onClick={handleTextToggle}>
+            Add Me
+          </button>
+          <button onClick={handleTextRemove}>Delete</button>
+          <button onClick={handleIncrement}>+</button>
+      </div>
+      <div className="container1">
+          <h1>{result}</h1>
+      </div>
     </div>
   );
 }
